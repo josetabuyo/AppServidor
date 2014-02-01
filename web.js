c8deea471885aabdb3f854ff1552f19ee1a8654f
José Miguel Tabuyo <josetabuyo@gmail.com>
2014-02-01 20:19:23 -0300

    con cross domain

--- web.js
+++ web.js
@@ -4,10 +4,21 @@ var app = express();
 var login = require('./login');
 app.use(login);
 
-var home = require('./home');
-app.use(home);
 
 
+//allowCrossDomain
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
+
+app.get('/', function(req, res){
+	res.send("default");
+	console.log('se ejecutó el action default');
+});
 
 var puerto = process.env.PORT || 3000;
 app.listen(puerto);
