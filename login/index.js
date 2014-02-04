c16711b46744b16d49cc61ac63b9efdc0e1bc21d
José Miguel Tabuyo <josetabuyo@gmail.com>
2014-02-03 23:06:16 -0300

    conectado a mongo en mongolabgit add --all

--- login/index.js
+++ login/index.js
@@ -2,6 +2,16 @@ var express = require('express');
 var app = module.exports =  express();
 
 
+var mongodb = require('mongodb');
+var uri = 'mongodb://josetabuyo:IvanTabuyo1020@ds030607.mongolab.com:30607/appdatabase';
+
+
+mongodb.MongoClient.connect(uri, function (err, db) {
+	console.log('conectamos');
+});
+
+
+
 app.get('/login', function(req, res){
 	res.send("Loginete logueao");
 	console.log('se ejecutó el action login');
