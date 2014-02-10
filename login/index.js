5f113451086e54de0c56606c04a41fe9a4e6c70f
José Miguel Tabuyo <josetabuyo@gmail.com>
2014-02-10 00:29:28 -0300

    usuarios con mongo a medias

--- login/index.js
+++ login/index.js
@@ -6,13 +6,63 @@ var mongodb = require('mongodb');
 var uri = 'mongodb://josetabuyo:IvanTabuyo1020@ds030607.mongolab.com:30607/appdatabase';
 
 
+var myCollection;
 mongodb.MongoClient.connect(uri, function (err, db) {
+	
+	myCollection = db.collection('test_collection');
 	console.log('conectamos');
 });
 
+app.use(express.bodyParser());
 
 
-app.get('/login', function(req, res){
-	res.send("Loginete logueao");
+
+app.post('/usuarioadd', function(req, res){
+	
+	myCollection.insert({
+		usuario: req.param('Usuario'),
+		password: req.param('Password')
+	}, function(err, result) {
+		if(err)
+			throw err;
+		
+		console.log("entry saved");
+	});
+	
+});
+
+app.post('/usuariologin', function(req, res){
+	
+	var cursor = myCollection.find({
+		usuario: req.param('Usuario'),
+		password: req.param('Password')
+	});
+	
+	
+	
+	
+	cursor.each(function(err, doc) {
+		if(err)
+			throw err;
+			
+		if(doc==null)
+			return;
+		
+		console.log(doc.usuario);
+		console.log(doc.password);
+	});
+	
+	
+	
+	
+	var salida = ''
+	+'tejuro que te validé '
+	+'el ' + req.param('Usuario')
+	+' y el ' + req.param('Password');
+	
+	
+	res.send( salida );
+	
 	console.log('se ejecutó el action login');
+	console.log('que vieneen el req', req);
 });
